d47f04fb338de574c275d138523f145c7486de35
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-29 23:41:42 +0100

    Added exit screen

---
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+}
