95e6eb3a3d8de5065eee24f0b9389687c80ceb41
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-30 23:35:12 +0100

    added initial state of choosing valkyrie

---
+++ Assets/Scripts/Player/GoToTargetPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToTargetPoint : MonoBehaviour
+{
+    public bool goToTargetPointEnabled = false;
+    public float movementSpeed = 3f;
+    private GameObject _targetPoint;
+    private Animator _animator;
+    void Start()
+    {
+        LoadComponent();
+    }
+
+    private void LoadComponent()
+    {
+        _targetPoint = GameObject.Find("PlayerTargetPoint");
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (goToTargetPointEnabled && _targetPoint.transform.position.x > transform.position.x)
+        {
+            transform.position += Vector3.right * Time.deltaTime * movementSpeed;
+            _animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            _animator.SetFloat("Speed", 0);
+        }
+    }
+}
