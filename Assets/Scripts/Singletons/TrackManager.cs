34cacf58e1782e53b894028de72d41df01d5e356
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-30 20:33:22 +0100

    Added track manager

---
+++ Assets/Scripts/Singletons/TrackManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackManager :  GenericSingletonClass<TrackManager>
+{
+    private AudioSource _audio;
+    public AudioClip mainTheme;
+    public AudioClip epilogueTheme;
+    void Start()
+    {
+        LoadComponents();
+        PlayMainTheme();
+    }
+
+    private void LoadComponents()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    public void PlayMainTheme()
+    {
+        _audio.Stop();
+        _audio.clip = mainTheme;
+        _audio.loop = true;
+        _audio.Play();
+    }
+    
+    public void PlayEpilogueTheme()
+    {
+        _audio.Stop();
+        _audio.clip = epilogueTheme;
+        _audio.loop = true;
+        _audio.Play();
+    }
+
+    void Update()
+    {
+        
+    }
+}
