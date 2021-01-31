53f783ee84fe50bca3f485150ce1665487e329a5
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-31 04:30:13 +0100

    Added deadzone

---
+++ Assets/Scripts/MapPoints/Deadzone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deadzone : MonoBehaviour
+{
+
+    private BoxCollider2D _box;
+    void Start()
+    {
+        LoadComponents();
+    }
+
+    private void LoadComponents()
+    {
+        _box = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerController>().Hurt();
+        }
+    }
+}
