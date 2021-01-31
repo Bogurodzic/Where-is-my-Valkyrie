e5ad8c99144939e41e37a54cb7d5aac498af52e2
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-31 02:00:57 +0100

    Fixed fireball collision

--- Assets/Scripts/FireballProjectile.cs
+++ Assets/Scripts/FireballProjectile.cs
@@ -18,7 +18,7 @@ public class FireballProjectile : MonoBehaviour
     }
      void OnTriggerEnter2D(Collider2D collision)
         {
-            if (!(collision.gameObject.tag == "Enemy"))
+            if (!(collision.gameObject.tag == "Enemy") && !(collision.gameObject.tag == "Collectible"))
             Destroy(this.gameObject);
         }
  }
